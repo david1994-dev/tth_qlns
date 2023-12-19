edd6334f1f516f104dfdbe58520a6921a03e40cb
david1994-dev <khanhhoang.dev@gmail.com>
2023-12-19 16:36:17 +0700

    integrate adminlte layout

--- app/Modules/Nhansu/resources/views/users/index.blade.php
+++ app/Modules/Nhansu/resources/views/users/index.blade.php
@@ -1,1 +1,23 @@
-<h1>Hello, I'am Hoang</h1>
+@extends('adminlte.Layout.app' )
+
+@section('metadata')
+@stop
+
+@section('styles')
+@stop
+
+@section('scripts')
+@stop
+
+@section('title')
+@stop
+
+@section('header')
+@stop
+
+@section('breadcrumb')
+@stop
+
+@section('content')
+    <h1>Hello, I'am Giap</h1>
+@stop
---
+++ resources/views/adminlte/Layout/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-------------------------------- Begin: Meta ----------------------------------->
+  @yield('metadata')
+  <!-------------------------------- End: Meta ----------------------------------->
+
+  <!-------------------------------- Begin: stylesheet ----------------------------------->
+  @include('adminlte.Layout.style')
+  @yield('styles')
+  <!-------------------------------- End: stylesheet ----------------------------------->
+
+</head>
+<body class="sidebar-mini">
+  <div class="wrapper">
+    <!-------------------------------- Begin: Header ----------------------------------->
+    @include('adminlte.Layout.header')
+    <!-------------------------------- End: Header ----------------------------------->
+
+    <!-------------------------------- Begin: Left Navigation ----------------------------------->
+    @include('adminlte.Layout.menu')
+    <!-------------------------------- End: Left Navigation ----------------------------------->
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper" style="min-height: 1302.26px;">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+
+      </section>
+
+      <!-- Main content -->
+      <div class="content">
+        @yield('content')
+      </div>
+      <!-- /.content -->
+    </div>
+
+    <!-------------------------------- Begin: Footer ----------------------------------->
+    @include('adminlte.Layout.footer')
+
+    <!-------------------------------- End: Footer ----------------------------------->
+  </div>
+<!-------------------------------- Begin: Script ----------------------------------->
+  @include('adminlte.Layout.scripts')
+  @yield('scripts')
+<!-------------------------------- End: Script ----------------------------------->
+</body>
+</html>
