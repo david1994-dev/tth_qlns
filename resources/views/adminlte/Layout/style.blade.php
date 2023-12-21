94c3ccf403592932c240e3396643748dcf24ea17
david1994-dev <khanhhoang.dev@gmail.com>
2023-12-21 16:09:53 +0700

    update asset link adminlte

--- resources/views/adminlte/Layout/style.blade.php
+++ resources/views/adminlte/Layout/style.blade.php
@@ -1,3 +1,2 @@
-<link rel="stylesheet" href="../../adminlte/plugins/fontawesome-free/css/all.min.css">
-<!-- Theme style -->
-<link rel="stylesheet" href="../../adminlte/dist/css/adminlte.min.css">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
