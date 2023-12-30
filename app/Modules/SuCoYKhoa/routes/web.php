0b88cdff6689f97f50cf3521f3951ad2d4eca376
david1994-dev <khanhhoang.dev@gmail.com>
2023-12-30 08:04:40 +0700

    refactor code

--- app/Modules/SuCoYKhoa/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('sucoykhoa')->middleware('web')->name('sucoykhoa.')->group(function () {
-    Route::middleware(['auth'])->group(function () {
-
-    });
-});
