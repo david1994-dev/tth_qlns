c2cc4805ed76f0336f842fe82621af8be629820a
TienTHM <106365827+minhtien41001@users.noreply.github.com>
2024-01-17 15:32:26 +0700

    update header footer menu

--- app/Modules/Nhansu/resources/views/nhan_vien/danh_sach.blade.php
+++ app/Modules/Nhansu/resources/views/nhan_vien/danh_sach.blade.php
@@ -1,46 +1,46 @@
 @extends('adminlte.Layout.app')
 @section('content')
-<div class="box box-primary">
-    <div class="box-header with-border">
-        <form action="">
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <input name="keyword" placeholder="Nhập mã chi nhánh, tên chi nhánh.." class="form-control"
-                            value="{{ $keyword ?? '' }}" />
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            <form action="">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <input name="keyword" placeholder="Nhập mã chi nhánh, tên chi nhánh.." class="form-control"
+                                value="{{ $keyword ?? '' }}" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Search</button>
-                    <a href="" class="btn btn-danger">Reset</a>
+                <div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Search</button>
+                        <a href="" class="btn btn-danger">Reset</a>
+                    </div>
+                </div>
+            </form>
+            <div class="row">
+                <div class="col-sm-6">
+                    <p style="display: inline-block;"><b></b> Nhân viên</p>
                 </div>
             </div>
-        </form>
-        <div class="row">
-            <div class="col-sm-6">
-                <p style="display: inline-block;"><b></b> Nhân viên</p>
-            </div>
         </div>
-    </div>
-    <div class="box-body card" style=" overflow-x: scroll; ">
-        <table class="table table-striped">
-            <thead>
-                <tr class="table table-striped">
-                    <th>STT</th>
-                    <th>Mã Nhân Viên</th>
-                    <th>Email</th>
-                    <th>Họ Và Tên</th>
-                    <th>Giới Tính</th>
-                    <th>Ngày Sinh</th>
-                    <th>Chi Nhánh</th>
-                    <th>Ngày Tạo</th>
-                    <th>QR</th>
-                    <th class="text-center">Thao Tác</th>
-                </tr>
-            </thead>
-            <tbody>
+        <div class="box-body card" style=" overflow-x: scroll; ">
+            <table class="table table-striped">
+                <thead>
+                    <tr class="table table-striped">
+                        <th>STT</th>
+                        <th>Mã Nhân Viên</th>
+                        <th>Email</th>
+                        <th>Họ Và Tên</th>
+                        <th>Giới Tính</th>
+                        <th>Ngày Sinh</th>
+                        <th>Chi Nhánh</th>
+                        <th>Ngày Tạo</th>
+                        <th>QR</th>
+                        <th class="text-center">Thao Tác</th>
+                    </tr>
+                </thead>
+                <tbody>
                     <tr>
                         <td>1</td>
                         <td>003437</td>
@@ -52,16 +52,14 @@
                         <td>17/01/2024</td>
                         <td></td>
                         <td class="text-center">
-                            <a class="btn btn-danger btn-sm delete-button"
-                                data-delete-url="">
+                            <a class="btn btn-danger btn-sm delete-button" data-delete-url="">
                                 Xóa <i class="bi bi-trash"></i>
                             </a>
-                            <a class="btn btn-primary btn-sm" target="_blank"
-                                href="">
+                            <a class="btn btn-primary btn-sm" target="_blank" href="">
                                 Sửa <i class="bi bi-pencil-square"></i>
                             </a>
                             <a target="_blank" href="" class="btn btn-primary btn-sm">
-                                  Chi tiết  <i class="bi bi-eye-fill"></i>
+                                Chi tiết <i class="bi bi-eye-fill"></i>
                             </a>
                         </td>
                     </tr>
@@ -76,16 +74,14 @@
                         <td>17/01/2024</td>
                         <td></td>
                         <td class="text-center">
-                            <a class="btn btn-danger btn-sm delete-button"
-                                data-delete-url="">
+                            <a class="btn btn-danger btn-sm delete-button" data-delete-url="">
                                 Xóa <i class="bi bi-trash"></i>
                             </a>
-                            <a class="btn btn-primary btn-sm" target="_blank"
-                                href="">
+                            <a class="btn btn-primary btn-sm" target="_blank" href="">
                                 Sửa <i class="bi bi-pencil-square"></i>
                             </a>
                             <a target="_blank" href="" class="btn btn-primary btn-sm">
-                                  Chi tiết  <i class="bi bi-eye-fill"></i>
+                                Chi tiết <i class="bi bi-eye-fill"></i>
                             </a>
                         </td>
                     </tr>
@@ -100,32 +96,30 @@
                         <td>17/01/2024</td>
                         <td></td>
                         <td class="text-center">
-                            <a class="btn btn-danger btn-sm delete-button"
-                                data-delete-url="">
+                            <a class="btn btn-danger btn-sm delete-button" data-delete-url="">
                                 Xóa <i class="bi bi-trash"></i>
                             </a>
-                            <a class="btn btn-primary btn-sm" target="_blank"
-                                href="">
+                            <a class="btn btn-primary btn-sm" target="_blank" href="">
                                 Sửa <i class="bi bi-pencil-square"></i>
                             </a>
                             <a target="_blank" href="" class="btn btn-primary btn-sm">
-                                  Chi tiết  <i class="bi bi-eye-fill"></i>
+                                Chi tiết <i class="bi bi-eye-fill"></i>
                             </a>
                         </td>
                     </tr>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
+        {{-- <div class="box-footer">
+            {!! \PaginationHelper::render(
+                $paginate['order'],
+                $paginate['direction'],
+                $paginate['offset'],
+                $paginate['limit'],
+                $count,
+                $paginate['baseUrl'],
+                [],
+            ) !!}
+        </div> --}}
     </div>
-    {{-- <div class="box-footer">
-        {!! \PaginationHelper::render(
-            $paginate['order'],
-            $paginate['direction'],
-            $paginate['offset'],
-            $paginate['limit'],
-            $count,
-            $paginate['baseUrl'],
-            [],
-        ) !!}
-    </div> --}}
-</div>
 @stop
